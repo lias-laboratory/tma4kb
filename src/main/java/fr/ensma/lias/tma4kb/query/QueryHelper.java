207765580486b50bb8d36dd0cd6083ee9ed17127
PARKIN Louise <parkinl@w.ensma.fr>
2020-08-10 11:20:43 +0200

    new comments for QueryHelper

--- src/main/java/fr/ensma/lias/tma4kb/query/QueryHelper.java
+++ src/main/java/fr/ensma/lias/tma4kb/query/QueryHelper.java
@@ -11,48 +11,48 @@ public interface QueryHelper {
 	/**
 	 * Executes a query and returns the number of answers
 	 * 
-	 * @param session
-	 * @return true if the query fails, false otherwise
+	 * @param session, k the threshold
+	 * @return number of answers
 	 */
 	int executeQuery(Session session, int k);
 	
 	/**
 	 * Executes a query using method ALL and returns the number of answers
 	 * 
-	 * @param session
-	 * @return true if the query fails, false otherwise
+	 * @param session, k the threshold
+	 * @return number of answers
 	 */
 	int execute_ALL(Session session, int k) throws IOException;
 	
 	/**
 	 * Executes a query using method STOP_K and returns the number of answers
 	 * 
-	 * @param session
-	 * @return true if the query fails, false otherwise
+	 * @param session, k the threshold
+	 * @return number of answers or k+1 if the number of answers exceeds the threshold
 	 */
 	int execute_STOPK(Session session, int k) throws IOException;
 	
 	/**
 	 * Executes a query using method COUNT and returns the number of answers
 	 * 
-	 * @param session
-	 * @return true if the query fails, false otherwise
+	 * @param session, k the threshold
+	 * @return number of answers
 	 */
 	int execute_COUNT(Session session, int k) throws IOException;
 	
 	/**
 	 * Executes a query using method LIMIT and returns the number of answers
 	 * 
-	 * @param session
-	 * @return true if the query fails, false otherwise
+	 * @param session, k the threshold
+	 * @return number of answers or k+1 if the number of answers exceeds the threshold
 	 */
 	int execute_LIMIT(Session session, int k) throws IOException;
 	
 	/**
 	 * Executes a query using method COUNT+LIMIT and returns the number of answers
 	 * 
-	 * @param session
-	 * @return true if the query fails, false otherwise
+	 * @param session, k the threshold
+	 * @return number of answers or k+1 if the number of answers exceeds the threshold
 	 */
 	int execute_COUNTLIMIT(Session session, int k) throws IOException;
 
